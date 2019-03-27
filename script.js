cd886a30c9bef764961cf07375fce1593073f496
Al-bat-ros <al-bat-ros@mail.ru>
2019-03-28 01:57:52 +0500

    Add js 1

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let tetris = document.createElement('div');
-tetris.classList.add('tetris');
+tetris.className = 'tetris'; //  или так tetris.classList.add('tetris')
 
-for (let i=1; i<181; i++) {
+for (let i=1; i<181; i++){
 	let excel = document.createElement('div');
-	excel.classList.add('excel');
+	excel.className = 'excel';  //  или так tetris.classList.add('excel')
+
+
 	tetris.appendChild(excel);
 }
 
-let main = document.getElementsByClassName('main')[0];
+let main = document.getElementsByClassName('main')[0]; // указываем что он первый жаже если он один.
+
 main.appendChild(tetris);
