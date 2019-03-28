aa319bbe8f2a658f584570512ca89efc45476636
Al-bat-ros <al-bat-ros@mail.ru>
2019-03-28 07:55:17 +0500

    add coments

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// создаём элемент 
 let tetris = document.createElement('div');
+// присваиваем к элементу класс
 tetris.className = 'tetris'; //  или так tetris.classList.add('tetris')
 
+// создаём сетку ячеек
 for (let i=1; i<181; i++){
 	let excel = document.createElement('div');
 	excel.className = 'excel';  //  или так tetris.classList.add('excel')
@@ -8,13 +11,15 @@ for (let i=1; i<181; i++){
 
 	tetris.appendChild(excel);
 }
-
+// выводим на экран
 let main = document.getElementsByClassName('main')[0]; // указываем что он первый жаже если он один.
 main.appendChild(tetris);
 
+
+// записываем в переменную все ячейки
 let excel = document.getElementsByClassName('excel');
 let i = 0;
-
+// присваиваем координаты каждой ячейке
 for (let y=18; y>0; y--){
 	for (let x=1; x<11; x++){
 		excel[i].setAttribute('posY', y);
@@ -23,3 +28,5 @@ for (let y=18; y>0; y--){
 		i++;
 	}
 }
+
+let x = 5, y = 10
