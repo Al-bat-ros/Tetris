33b56d3b2b69d33cf1696c035e7bbc17e615ca20
Al-bat-ros <al-bat-ros@mail.ru>
2019-03-28 13:07:32 +0500

    add figure

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ tetris.className = 'tetris'; //  или так tetris.classList.add('tetris')
 // создаём сетку ячеек
 for (let i=1; i<181; i++){
 	let excel = document.createElement('div');
-	excel.className = 'excel';  //  или так tetris.classList.add('excel')
+	excel.className = 'excel';  //  или так excel.classList.add('excel')
 
 
 	tetris.appendChild(excel);
@@ -22,9 +22,9 @@ let i = 0;
 // присваиваем координаты каждой ячейке
 for (let y=18; y>0; y--){
 	for (let x=1; x<11; x++){
-		excel[i].setAttribute('posY', y);
 		excel[i].setAttribute('posX', x);
-
+		excel[i].setAttribute('posY', y);
+		
 		i++;
 	}
 }
@@ -37,11 +37,54 @@ let mainArr = [
   		[0,1],
   		[0,2],
   		[0,3]
-  ]
+  ],
   // квадрат
   [
   		[1,0],
   		[0,1],
   		[1,1]
+  ],
+  // угол левый
+  [
+        [1,0],
+  		[0,1],
+  		[0,2]
+  ],
+  // угол правый
+    [
+        [1,0],
+  		[1,1],
+  		[1,2]
   ]
+
 ]
+
+let currentFigure = 0;
+let figureBody = 0;
+
+ function create() {
+ 	// выбираем случайное число 
+	 function getRandom() {
+	 	return Math.round(Math.random()*(mainArr.length-1));
+		 }
+     // записываем его в переменную
+	 currentFigure = getRandom();
+
+	 // создаём случайную деталь
+	 figureBody = [
+
+	     document.querySelector(`[posX = "${x}"][posY = "${y}"]`),
+	     document.querySelector(`[posX = "${x + mainArr[currentFigure][0][0]}"][posY = "${y + mainArr[currentFigure][0][1]}"]`),
+	     document.querySelector(`[posX = "${x + mainArr[currentFigure][1][0]}"][posY = "${y + mainArr[currentFigure][1][1]}"]`),
+         document.querySelector(`[posX = "${x + mainArr[currentFigure][2][0]}"][posY = "${y + mainArr[currentFigure][2][1]}"]`)
+         
+         ]
+      // присваеваем ей класс
+	for (let i=0; i<figureBody.length; i++) {
+		//figureBody[i].className = 'figure'; 
+		 figureBody[i].classList.add('figure');
+	}	
+ }
+
+ create(); 
+
