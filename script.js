04d8caf1e28c5971b1df26d134fbf8104370c55c
Al-bat-ros <al-bat-ros@mail.ru>
2019-03-28 05:48:17 +0500

    Add excel position Y,X

--- script.js
+++ script.js
@@ -10,5 +10,16 @@ for (let i=1; i<181; i++){
 }
 
 let main = document.getElementsByClassName('main')[0]; // указываем что он первый жаже если он один.
-
 main.appendChild(tetris);
+
+let excel = document.getElementsByClassName('excel');
+let i = 0;
+
+for (let y=18; y>0; y--){
+	for (let x=1; x<11; x++){
+		excel[i].setAttribute('posY', y);
+		excel[i].setAttribute('posX', x);
+
+		i++;
+	}
+}
